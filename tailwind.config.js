916ef7dbc6328f24acfb11a15128f3258a44186e
recursiveway <ashutoshchoudhary998@gmail.com>
2024-07-22 00:25:14 +0530

    setup

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+content: [
+  "./App.{js,jsx,ts,tsx}", 
+  "./app/**/*.{jsx,tsx}",
+  "./**/*.{jsx,tsx}"
+],  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
